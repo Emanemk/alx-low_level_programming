9f17a1d2df4211427d0bbe1d251a059aa143e690
emanemk <eman.a.karrar@gmail.com>
2022-12-11 15:26:16 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,20 @@
-:
+#include<stdio>
+
+/**
+ * main - entry point
+ *
+ * description: print a string
+ *
+ * Return: return value of main is 0 (success)
+ *
+ */
+
+int main(void)
+{
+
+	char mystr[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", mystr);
+
+	return (0);
+}
