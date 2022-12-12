258ab269e82f60292b008f942049b7c61e858620
emanemk <eman.a.karrar@gmail.com>
2022-12-11 16:15:52 -0800

    sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+/**
+ * main - entry point 
+ *
+ * descriptio: print sizes of types
+ *
+ * Return: return value of main is 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char charType;
+	int intType;
+	long int longintType;
+	long long int longlongintType;
+	float floatType;
+
+	printf("Size of char: %c byte\n", sizeof(charType));
+	printf("Size of int: %d byte\n", sizeof(intType));
+	printf("Size of long int: %ld byte\n", sizeof(longintType));
+	printf("Size of long long int: %lld byte\n", sizeof(longlongintType));
+	printf("Size of float: %f byte\n", sizeof(floatType));
+
+	return (0);
+}
+	
+	
