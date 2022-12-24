042e328aebb3ee2cf27d490f712844994e99671b
emanemk <eman.a.karrar@gmail.com>
2022-12-23 18:39:24 -0800

    infinite_add

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * infinite_add - adds num
+ * @n1: first num
+ * @n2: second num
+ * @size_r: buffer size
+ * @r: parameter
+ * Return: returns pointer to result
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int num, tens = 0;
+
+	for (; *n1 && *n2; n1--, n2--, r_index--)
+	{
+		num = (*n1 - '0') + (*n2 - '0');
+		num += tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+
+	for (; *n1; n1--; r_index++)
+	{
+		num = *(n1 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+	for (; *n2; n2--; r_index--)
+	{
+		num = (*n2 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+}
